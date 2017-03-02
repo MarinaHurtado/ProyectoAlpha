39912a108af3bed8cab1c2b3e2f09bcbabd8a549
MarinaHurtado <mmhhvv1@gmail.com>
2017-03-02 15:59:20 -0600

    Game interface

--- src/client/Mole.java
+++ src/client/Mole.java
@@ -27,17 +27,92 @@ public class Mole extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        mole1 = new javax.swing.JButton();
+        mole2 = new javax.swing.JButton();
+        mole3 = new javax.swing.JButton();
+        mole4 = new javax.swing.JButton();
+        mole5 = new javax.swing.JButton();
+        mole6 = new javax.swing.JButton();
+        mole7 = new javax.swing.JButton();
+        mole8 = new javax.swing.JButton();
+        mole9 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(51, 153, 0));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
+        jLabel1.setText("Whack a mole");
+
+        mole1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
+
+        mole9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/client/hole.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(mole1)
+                    .addComponent(mole4)
+                    .addComponent(mole7))
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(mole5)
+                    .addComponent(mole2)
+                    .addComponent(mole8))
+                .addGap(42, 42, 42)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(mole3)
+                    .addComponent(mole6)
+                    .addComponent(mole9))
+                .addContainerGap(31, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(58, 58, 58))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(jLabel1)
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(mole1)
+                                    .addComponent(mole2)
+                                    .addComponent(mole3))
+                                .addGap(27, 27, 27)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(mole5)
+                                    .addComponent(mole6)))
+                            .addComponent(mole4))
+                        .addGap(30, 30, 30)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(mole8)
+                            .addComponent(mole9)))
+                    .addComponent(mole7))
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
         pack();
@@ -79,5 +154,15 @@ public class Mole extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton mole1;
+    private javax.swing.JButton mole2;
+    private javax.swing.JButton mole3;
+    private javax.swing.JButton mole4;
+    private javax.swing.JButton mole5;
+    private javax.swing.JButton mole6;
+    private javax.swing.JButton mole7;
+    private javax.swing.JButton mole8;
+    private javax.swing.JButton mole9;
     // End of variables declaration//GEN-END:variables
 }
